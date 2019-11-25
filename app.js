ca2a3459e574c68401f506b404c9c674b9f882ba
Denis Bogatyev <denis3509@gmail.com>
2019-11-25 17:31:45 +0300

    prod

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const checkAuth = require('./middleware/checkAuth').checkAuth;
 const chat = require('./chat');
 const cors = require('cors');
 
-app.use(cors({origin : 'http://localhost:3000'}));
+app.use(cors({origin : config.reactHostName }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -30,12 +30,12 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", config.reactHostName);
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
-  next();
-});
+//
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", config.reactHostName);
+//   res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+//   next();
+// });
 
 app.use('/index', (req, res, next) => {
   res.send(req.query);
