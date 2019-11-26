bede73f6489b97bb16c4f5347b35cfdc112e90ee
Denis Bogatyev <denis3509@gmail.com>
2019-11-26 15:48:57 +0300

    prod

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const index = {
     httpOnly: false,
     maxAge: 24 * 60 * 60 * 1000,
     secure: true,
-    domain: 'https://study-react-app.herokuapp.com',
+    domain: 'https://study-nodejs-app.herokuapp.com',
   },
   reactHostName : 'https://study-react-app.herokuapp.com',
   mongodbConnectionURI :'mongodb://admin:adminN9*@ds061797.mlab.com:61797/study-project-mongodb',
